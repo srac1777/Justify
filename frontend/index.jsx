7ebae341521b78aaf9c57dce1acd00e78ad27238
srac1777 <shashank.racherla@gmail.com>
2018-10-08 14:08:35 -0700

    redux

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
     ReactDOM.render(<h1>welcome to Justify</h1>, root);
+
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
+window.login = login;
+window.signup = signup;
+window.logout = logout;
