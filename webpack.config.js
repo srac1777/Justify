7333c2bdc79acf7970a22eb1fdd3a7e82deb909e
srac1777 <shashank.racherla@gmail.com>
2018-10-08 12:06:24 -0700

    react working

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     query: {
-                        presets: ['env', 'react']
+                        presets: ['@babel/env', '@babel/react']
                     }
                 },
             }
