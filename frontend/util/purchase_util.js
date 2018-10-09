18e822f54a99386d91a14a1be4b4b610986026ca
srac1777 <shashank.racherla@gmail.com>
2018-10-09 14:56:33 -0700

    util done

---
+++ frontend/util/purchase_util.js
@@ -0,0 +1,38 @@
+export const fetchPurchases = () => {
+    return $.ajax({
+        method: 'GET',
+        url: 'api/purchases'
+    });
+};
+
+export const fetchPurchase = id => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/purchases/${id}`
+    });
+};
+
+export const createPlaylist = purchase => {
+    // console.log(purchase);
+
+    return $.ajax({
+        url: 'api/purchases',
+        method: 'POST',
+        data: { purchase }
+    });
+};
+
+export const updatePurchase = (purchase) => {
+    return $.ajax({
+        url: `api/purchases/${purchase.id}`,
+        method: 'PATCH',
+        data: { purchase }
+    });
+};
+
+export const deletePurchase = id => {
+    return $.ajax({
+        url: `api/purchases/${id}`,
+        method: 'DELETE'
+    });
+};
